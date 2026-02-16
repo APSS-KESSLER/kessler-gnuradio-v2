4acc1f37b72743a823320d2217df13347143923c
Joshua-Systems <eejl609@aucklanduni.ac.nz>
2026-02-16 18:32:49 +1300

    Fix: Fixed Async and GNURadio Install for Linux Ubuntu

---
+++ Transmission_Graphs/tx_rx.py
@@ -0,0 +1,379 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: flat-sat-workbench
+# GNU Radio version: 3.10.12.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import blocks, gr
+from gnuradio import digital
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import network
+from gnuradio import pdu
+from gnuradio import soapy
+import math
+import satellites.components.deframers
+import satellites.components.demodulators
+import sip
+import threading
+
+
+
+class tx_rx(gr.top_block, Qt.QWidget):
+
+    def __init__(self, baud=9600):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "tx_rx")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        self.flowgraph_started = threading.Event()
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.baud = baud
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.mod_index = mod_index = 0.5
+        self.samp_rate = samp_rate = 2e6
+        self.freq_deviation = freq_deviation = (mod_index * baud) / 2
+        self.sensitivity = sensitivity = (2 * math.pi * freq_deviation) / samp_rate
+        self.num_preamble_aa = num_preamble_aa = 5
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.soapy_rtlsdr_source_0 = None
+        dev = 'driver=rtlsdr'
+        stream_args = 'bufflen=16384'
+        tune_args = ['']
+        settings = ['']
+
+        def _set_soapy_rtlsdr_source_0_gain_mode(channel, agc):
+            self.soapy_rtlsdr_source_0.set_gain_mode(channel, agc)
+            if not agc:
+                  self.soapy_rtlsdr_source_0.set_gain(channel, self._soapy_rtlsdr_source_0_gain_value)
+        self.set_soapy_rtlsdr_source_0_gain_mode = _set_soapy_rtlsdr_source_0_gain_mode
+
+        def _set_soapy_rtlsdr_source_0_gain(channel, name, gain):
+            self._soapy_rtlsdr_source_0_gain_value = gain
+            if not self.soapy_rtlsdr_source_0.get_gain_mode(channel):
+                self.soapy_rtlsdr_source_0.set_gain(channel, gain)
+        self.set_soapy_rtlsdr_source_0_gain = _set_soapy_rtlsdr_source_0_gain
+
+        def _set_soapy_rtlsdr_source_0_bias(bias):
+            if 'biastee' in self._soapy_rtlsdr_source_0_setting_keys:
+                self.soapy_rtlsdr_source_0.write_setting('biastee', bias)
+        self.set_soapy_rtlsdr_source_0_bias = _set_soapy_rtlsdr_source_0_bias
+
+        self.soapy_rtlsdr_source_0 = soapy.source(dev, "fc32", 1, '',
+                                  stream_args, tune_args, settings)
+
+        self._soapy_rtlsdr_source_0_setting_keys = [a.key for a in self.soapy_rtlsdr_source_0.get_setting_info()]
+
+        self.soapy_rtlsdr_source_0.set_sample_rate(0, samp_rate)
+        self.soapy_rtlsdr_source_0.set_frequency(0, 435E6)
+        self.soapy_rtlsdr_source_0.set_frequency_correction(0, 97.5)
+        self.set_soapy_rtlsdr_source_0_bias(bool(False))
+        self._soapy_rtlsdr_source_0_gain_value = 20
+        self.set_soapy_rtlsdr_source_0_gain_mode(0, bool(False))
+        self.set_soapy_rtlsdr_source_0_gain(0, 'TUNER', 20)
+        self.soapy_hackrf_sink_0 = None
+        dev = 'driver=hackrf'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+
+        self.soapy_hackrf_sink_0 = soapy.sink(dev, "fc32", 1, '',
+                                  stream_args, tune_args, settings)
+        self.soapy_hackrf_sink_0.set_sample_rate(0, samp_rate)
+        self.soapy_hackrf_sink_0.set_bandwidth(0, 0)
+        self.soapy_hackrf_sink_0.set_frequency(0, 435e6)
+        self.soapy_hackrf_sink_0.set_gain(0, 'AMP', False)
+        self.soapy_hackrf_sink_0.set_gain(0, 'VGA', min(max(30, 0.0), 47.0))
+        self.satellites_fsk_demodulator_0 = satellites.components.demodulators.fsk_demodulator(baudrate = baud, samp_rate = baud*2, iq = True, subaudio = False, options="--deviation 2400 --use_agc")
+        self.satellites_endurosat_deframer_0 = satellites.components.deframers.endurosat_deframer(syncword_threshold=0, options="")
+        self.qtgui_waterfall_sink_x_2 = qtgui.waterfall_sink_f(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            435e6, #fc
+            samp_rate, #bw
+            "FINAL", #name
+            0, #number of inputs
+            None # parent
+        )
+        self.qtgui_waterfall_sink_x_2.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_2.enable_grid(False)
+        self.qtgui_waterfall_sink_x_2.enable_axis_labels(True)
+
+
+        self.qtgui_waterfall_sink_x_2.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_2.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_2.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_2.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_2.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_2.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_2_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_2.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_2_win)
+        self.qtgui_waterfall_sink_x_0_0 = qtgui.waterfall_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            435e6, #fc
+            samp_rate, #bw
+            "RAW RF INPUT", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_waterfall_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0_0.enable_axis_labels(True)
+
+
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0_0.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_0_win)
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            435e6, #fc
+            samp_rate, #bw
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+
+
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.pdu_pdu_to_stream_x_0 = pdu.pdu_to_stream_b(pdu.EARLY_BURST_BALK, 64)
+        self.network_socket_pdu_0_0 = network.socket_pdu('UDP_SERVER', '127.0.0.2', '2000', 10000, False)
+        self.network_socket_pdu_0 = network.socket_pdu('UDP_CLIENT', '127.0.0.1', '2003', 10000, False)
+        self.digital_symbol_sync_xx_0 = digital.symbol_sync_cc(
+            digital.TED_MUELLER_AND_MULLER,
+            (samp_rate/baud),
+            0.045,
+            1.0,
+            1.0,
+            1.5,
+            2,
+            digital.constellation_bpsk().base(),
+            digital.IR_MMSE_8TAP,
+            128,
+            [])
+        self.digital_gfsk_mod_0 = digital.gfsk_mod(
+            samples_per_symbol=(int(samp_rate // baud)),
+            sensitivity=sensitivity,
+            bt=0.35,
+            verbose=False,
+            log=False,
+            do_unpack=True)
+        self.blocks_message_debug_0_0 = blocks.message_debug(True, gr.log_levels.info)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.network_socket_pdu_0_0, 'pdus'), (self.pdu_pdu_to_stream_x_0, 'pdus'))
+        self.msg_connect((self.satellites_endurosat_deframer_0, 'out'), (self.blocks_message_debug_0_0, 'print_pdu'))
+        self.msg_connect((self.satellites_endurosat_deframer_0, 'out'), (self.network_socket_pdu_0, 'pdus'))
+        self.msg_connect((self.satellites_endurosat_deframer_0, 'out'), (self.qtgui_waterfall_sink_x_2, 'in'))
+        self.connect((self.digital_gfsk_mod_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.digital_gfsk_mod_0, 0), (self.soapy_hackrf_sink_0, 0))
+        self.connect((self.digital_symbol_sync_xx_0, 0), (self.satellites_fsk_demodulator_0, 0))
+        self.connect((self.pdu_pdu_to_stream_x_0, 0), (self.digital_gfsk_mod_0, 0))
+        self.connect((self.satellites_fsk_demodulator_0, 0), (self.satellites_endurosat_deframer_0, 0))
+        self.connect((self.soapy_rtlsdr_source_0, 0), (self.digital_symbol_sync_xx_0, 0))
+        self.connect((self.soapy_rtlsdr_source_0, 0), (self.qtgui_waterfall_sink_x_0_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "tx_rx")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_baud(self):
+        return self.baud
+
+    def set_baud(self, baud):
+        self.baud = baud
+        self.set_freq_deviation((self.mod_index * self.baud) / 2)
+        self.digital_symbol_sync_xx_0.set_sps((self.samp_rate/self.baud))
+
+    def get_mod_index(self):
+        return self.mod_index
+
+    def set_mod_index(self, mod_index):
+        self.mod_index = mod_index
+        self.set_freq_deviation((self.mod_index * self.baud) / 2)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_sensitivity((2 * math.pi * self.freq_deviation) / self.samp_rate)
+        self.digital_symbol_sync_xx_0.set_sps((self.samp_rate/self.baud))
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(435e6, self.samp_rate)
+        self.qtgui_waterfall_sink_x_0_0.set_frequency_range(435e6, self.samp_rate)
+        self.qtgui_waterfall_sink_x_2.set_frequency_range(435e6, self.samp_rate)
+        self.soapy_hackrf_sink_0.set_sample_rate(0, self.samp_rate)
+        self.soapy_rtlsdr_source_0.set_sample_rate(0, self.samp_rate)
+
+    def get_freq_deviation(self):
+        return self.freq_deviation
+
+    def set_freq_deviation(self, freq_deviation):
+        self.freq_deviation = freq_deviation
+        self.set_sensitivity((2 * math.pi * self.freq_deviation) / self.samp_rate)
+
+    def get_sensitivity(self):
+        return self.sensitivity
+
+    def set_sensitivity(self, sensitivity):
+        self.sensitivity = sensitivity
+
+    def get_num_preamble_aa(self):
+        return self.num_preamble_aa
+
+    def set_num_preamble_aa(self, num_preamble_aa):
+        self.num_preamble_aa = num_preamble_aa
+
+
+
+def argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--baud", dest="baud", type=eng_float, default=eng_notation.num_to_str(float(9600)),
+        help="Set baud [default=%(default)r]")
+    return parser
+
+
+def main(top_block_cls=tx_rx, options=None):
+    if options is None:
+        options = argument_parser().parse_args()
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(baud=options.baud)
+
+    tb.start()
+    tb.flowgraph_started.set()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
