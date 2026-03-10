4ae7ae89fbbf1222a172cb474570ab4be5913d55
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2026-03-10 16:27:06 +1300

    Fix: CPU arch detection on install of conda

--- Installation Guides/install.sh
+++ Installation Guides/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Only using the populer distrobutions
-# If you are not using any of these, then you are smart enough to get these dependancies. 
 readonly arch="arch"
 readonly fedora="fedora"
 readonly debian="debian"
 readonly ubuntu="ubuntu"
 
-readonly os_name=$(cat /etc/os-release | grep -w ID | awk -F = '{print $2}')
 
+readonly cpu=$(uname -m)
+readonly os_name=$(cat /etc/os-release | grep -w ID | awk -F = '{print $2}')
 if [ $? -ne 0 ]; then
     echo "Unexpected error, exiting..."
     exit 1
@@ -18,11 +18,11 @@ read -r -p "RECOMMENDED: Do you want to use conda [Yn]: " conda
 conda=${conda,,}
 if [[ "$conda" =~ ^([yes|y])$ ]]; then
     echo "Installing conda"
-    if [[ "$(arch)" == "aarch64" ]]; then                                                              
+    if [[ "$cpu" == "aarch64" ]]; then
         wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
         chmod +x ./Miniconda3-latest-Linux-aarch64.sh
         ./Miniconda3-latest-Linux-aarch64.sh
-    elif [[ "$(arch)" == "x86_64" ]]; then
+    elif [[ "$cpu" == "x86_64" ]]; then
         wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
         chmod +x ./Miniconda3-latest-Linux-x86_64.sh
         ./Miniconda3-latest-Linux-x86_64.sh
@@ -45,13 +45,13 @@ echo "Installing via system packages..."
 if [[ $os_name == $arch ]]; then
     while true; do
         read -p "(Arch Linux) This will install system wide python packages, do you want to continue? [yN]" yn
-        case "$yn" in 
+        case "$yn" in
             [Yy]* ) break;;
             * ) echo "Exiting. Use conda install instead"; exit 1;;
         esac
     done
     yay -S python gnuradio gnuradio-companion gnuradio-osmodr soapyrtlsdr gr-sattlies
-    if [$? -eq 127]; then 
+    if [$? -eq 127]; then
         echo "yay is not detected, using pacman and makepkg"
         sudo pacman -S python gnuradio gnuradio-companion gnuradio-osmodr soapyrtlsdr git
         if [$? -ne 0]; then
